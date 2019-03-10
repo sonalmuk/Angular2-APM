bced1325b7798abb292a1571fdeef10a788e27e7
Sonal <sonal_muk@hotmail.com>
2019-03-10 18:29:51 -0400

    angular js project from plural site

---
+++ APM/src/app/products/products.ts
@@ -0,0 +1,11 @@
+export interface IProducts {
+     productId : number;
+     productName :  string ;
+     productCode :  string; 
+     releaseDate :  Date;
+     description :  string;
+     price : number;
+     starRating : number;
+     imageUrl :  string;
+
+}
